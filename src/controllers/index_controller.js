75a93d8ddd0f81f0a7b887db6225216e64fc50e5
Andrew <anxsaiyuki@gmail.com>
2016-10-15 11:19:13 -0700

    Intial establish for project

---
+++ src/controllers/index_controller.js
@@ -0,0 +1,9 @@
+indexApp.controller('index.controller', function($scope, PlayerInfo) {
+
+	$scope.search_input = '';
+
+  $scope.search = function() {
+    console.log($scope.search_input);
+  };
+
+});
